5fc0b59c6b7df3ebbb562b4ccf6dd2cfd285a5f7
Lucas Henrique <84149138+LukC30@users.noreply.github.com>
2024-04-10 12:13:39 -0300

    "script banco"

--- bot/db_binance.sql
+++ bot/db_binance.sql
@@ -5,9 +5,11 @@ create table tbl_users(
 id int unsigned auto_increment primary key,
 api_key varchar(100) not null,
 api_secret varchar(100) not null,
+email varchar(100),
+pass varchar(100),
 constraint unique (api_key),
-constraint unique (api_secret)
-
+constraint unique (api_secret),
+constraint unique (email)
 
 );
 
@@ -17,11 +19,15 @@ id int auto_increment primary key,
 api_key varchar(100),
 ativo varchar(20) not null,
 tipo_de_ordem enum ('BUY', 'SELL'),
-valor_depositado decimal(7,6) not null,
-ultimo_alvo decimal(7,6) not null,
+valor_depositado decimal(12,6) not null,
+quantidade_ativo decimal (12,6) not null,
+ultimo_alvo decimal(12,6) not null,
+preco_entrada decimal(12,6) not null,
+preco_parada decimal(12,6) not null,
 ativa enum('n', 's'),
-order_id varchar(100)
-
+order_id varchar(100),
+foreign key (api_key)
+references tbl_users(api_key)
 
 );
 
